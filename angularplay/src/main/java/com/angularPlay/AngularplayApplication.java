3e0e619386e0c1a94fa796fa1c8b630f9b42f095
Miguel Ángel <mdiasan28@gmail.com>
2021-06-09 19:04:42 +0200

    Cambios de configuracion Heroku

--- angularplay/src/main/java/com/angularPlay/AngularplayApplication.java
+++ angularplay/src/main/java/com/angularPlay/AngularplayApplication.java
@@ -7,6 +7,18 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @ServletComponentScan 
 @SpringBootApplication
+//spring.datasource.url= jdbc:mysql://localhost:3306/angularplay?serverTimezone=Europe/Madrid
+//spring.datasource.username=angularplay
+//spring.datasource.password=1234
+//spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+//spring.jpa.database-platform = org.hibernate.dialect.MySQL5Dialect
+//spring.jpa.generate-ddl=true
+//spring.jpa.hibernate.ddl-auto = update
+//<dependency>
+//<groupId>mysql</groupId>
+//<artifactId>mysql-connector-java</artifactId>
+//<scope>runtime</scope>
+//</dependency>
 public class AngularplayApplication {
 
 	public static void main(String[] args) {
